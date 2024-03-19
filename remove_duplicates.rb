331db7fb76094e55e11a518e3ac34eaad0730615
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-03-19 10:54:38 +0545

    adding remove_duplicates.rb

---
+++ remove_duplicates.rb
@@ -0,0 +1,33 @@
+unique([])
+def unique(integers)
+    return integers.uniq 
+end
+
+puts unique([1, 5, 2, 0, 2, -3, 1, 10])
+
+
+# Define the unique method that removes duplicates
+# def unique(integers)
+#     # unique_value=Set.new([integers])
+#     unique_value=integers.to_set
+#     if (unique_value == Set.new([])) 
+#         puts "[]"     
+#     else
+#         unique_value.each{|e| puts e}
+#     end
+    
+# end
+
+
+# def unique(integers)
+#     a=Array.new
+#     while (i <= integers.length)
+      
+#     end
+    
+# end
+
+
+
+
+
